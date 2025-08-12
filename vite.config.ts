8711c9a132b20c14bd1c7978e026db4e84647d38
Rafael Lemos <rafael.penhalemos@gmail.com>
2025-08-12 19:02:26 -0400

    configurando o deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/personal-portfolio-main",
+  base: "/personal-portfolify-main",
   server: {
     host: "::",
     port: 8080,
